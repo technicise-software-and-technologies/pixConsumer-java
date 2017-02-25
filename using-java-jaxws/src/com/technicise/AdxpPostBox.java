c25ea9756db2656f3357a3e1fd15bc21ae8b96c9
sayan801 <sayan801@gmail.com>
2017-02-25 12:13:33 +0530

    changing package name

---
+++ using-java-jaxws/src/com/technicise/AdxpPostBox.java
@@ -0,0 +1,36 @@
+
+package com.technicise;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for adxp.postBox complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="adxp.postBox">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{urn:hl7-org:v3}ADXP">
+ *       &lt;attribute name="partType" type="{urn:hl7-org:v3}AddressPartType" fixed="POB" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "adxp.postBox")
+public class AdxpPostBox
+    extends ADXP
+    implements Serializable
+{
+
+    private final static long serialVersionUID = 20130510160300L;
+
+}
